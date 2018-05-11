a4f603e129a9d0665b8de0ca59d6bd8eb55ba23e
Alin Epure <alinepure@reea.net>
2018-05-11 11:01:30 +0300

    Add DB connection for production

--- src/League/OAuth2/Server/Classes/DBConnection.php
+++ src/League/OAuth2/Server/Classes/DBConnection.php
@@ -9,9 +9,27 @@ class DBConnection {
     private static $dsn = 'mysql:dbname=oauth_db;host=127.0.0.1';
     private static $user = 'root';
     private static $password = 'cheatgrass';
-//    private static $password = '@linr33@!';
 
     public static function connect() {
+        $ComputerName = '';
+
+        if (isset($_SERVER['COMPUTERNAME'])) {
+            $ComputerName=$_SERVER['COMPUTERNAME'];
+        }
+
+        if (isset($_ENV['COMPUTERNAME'])) {
+            $ComputerName=$_ENV['COMPUTERNAME'];
+        }
+
+        $ComputerName=strtolower($ComputerName);
+
+        if ($ComputerName == 'do-api1') {
+            DBConnection::$dsn = 'mysql:dbname=oauth_db;host=citscidb1';
+            DBConnection::$password = 'avqXxCs4jm';
+        } else if ($ComputerName == 'aline') {
+            DBConnection::$password = '@linr33@!';
+        }
+
         try {
             $dbConn = new PDO(DBConnection::$dsn, DBConnection::$user, DBConnection::$password);
         } catch (PDOException $e) {
